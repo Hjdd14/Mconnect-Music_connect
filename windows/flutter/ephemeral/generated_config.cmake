0b8759de154c68c27ba9e953814b545cb6579de9
Hjdd14 <2443599899@qq.com>
2026-05-31 11:52:16 +0800

    修复息屏播放，加强歌词体验

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\PC\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Code_Work\\My_Projects\\Mconnect-Music_connect" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.1.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\PC\\flutter\\flutter"
+  "PROJECT_DIR=D:\\Code_Work\\My_Projects\\Mconnect-Music_connect"
+  "FLUTTER_ROOT=C:\\Users\\PC\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Code_Work\\My_Projects\\Mconnect-Music_connect\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Code_Work\\My_Projects\\Mconnect-Music_connect"
+  "FLUTTER_TARGET=lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuMzguNA==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049NjZkZDkzZjlhMg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YTVjYjk2MzY5ZQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMC4z"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=true"
+  "PACKAGE_CONFIG=D:\\Code_Work\\My_Projects\\Mconnect-Music_connect\\.dart_tool\\package_config.json"
+)
